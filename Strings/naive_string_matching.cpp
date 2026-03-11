c12f1dc6c1fad5e9793bc65ba6b2cad3fc1587e7
CodeBloodMe <krishkaushik5178@gmail.com>
2026-03-11 10:51:19 +0530

    Implement naive string matching algorithm

--- Strings/naive_string_matching.cpp
+++ Strings/naive_string_matching.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+void naiveStringMatch(string text,string pattern){
+
+    int n=text.length();
+    int m=pattern.length();
+                                        
+
+    for(int i=0;i<=n-m;i++){
+        int j;
+        for( j=0;j<m;j++){
+            if(text[i+j]!=pattern[j]){
+                break;
+            }
+            
+        }
+        if(j==m){
+            cout<<"pattern found at "<<i<<endl;;
+        }
+    }
+    
+}
+int main(){
+    string text="hello world";
+    string pattern="lo ";
+
+    naiveStringMatch(text,pattern);
+}
